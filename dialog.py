a6c3d44eaa5aa77f1cb65ac1a10b2bdb05e8cc4d
Dhruvesh Borad <104754981+dhruveshborad@users.noreply.github.com>
2022-06-06 20:53:04 +0530

    calendar

--- dialog.py
+++ dialog.py
@@ -1,18 +1,19 @@
 import sys
-from PyQt5 import QtWidgets
-from PyQt5.QtWidgets import QDialog, QApplication
+import sqlite3
+from PyQt5 import QtWidgets , QtCore
+from PyQt5.QtWidgets import QDialog, QApplication,QMessageBox,QListWidgetItem, QCheckBox,QCalendarWidget
 from PyQt5.uic import loadUi
 import pymongo
 import random
 import math
 import smtplib
-from email.message import EmailMessage
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 from email.mime.base import MIMEBase
 import os.path
 from email import encoders
-
+from PyQt5.QtGui import *
+from PyQt5.QtCore import *
 
 class Login(QDialog):
     def __init__(self):
@@ -30,9 +31,15 @@ class Login(QDialog):
         mydb = client['mydb']
         mycol = mydb["people"]
         data = {'name': email, 'password': password}
-
         if mycol.find_one(data):
             print("Successfully logged in with email: ", email, "and password:", password)
+            calendar = Calendar()
+            widget.addWidget(calendar)
+            widget.setCurrentIndex(widget.currentIndex() + 1)
+            global loginid
+            loginid = ""
+            loginid = self.lineEditusername.text()
+            print(email)
         else:
             print("username not found")
 
@@ -192,12 +199,63 @@ class ForgotPass(QDialog):
         widget.addWidget(login)
         widget.setCurrentIndex(widget.currentIndex() + 1)
 
+class Calendar(QDialog):
+
+    def __init__(self):
+        super(Calendar, self).__init__()
+        loadUi("form2.ui", self)
+        self.calendarWidget.selectionChanged.connect(self.calendarDateChanged)
+        self.saveButton.clicked.connect(self.saveChanges)
+        self.addButton.clicked.connect(self.addNewTask)
+
+    def calendarDateChanged(self):
+        print("The calendar date was changed.")
+        dateSelected = self.calendarWidget.selectedDate().toPyDate()
+        print("Date selected:", dateSelected)
+        self.updateTaskList(dateSelected)
+
+    def updateTaskList(self, date):
+        self.tasksListWidget.clear()
+        client = pymongo.MongoClient()
+        mydb = client['mydb']
+        mycol = mydb["people"]
+        data = {str(date):[],'name':(loginid)}
+        if mycol.find_one({'name':loginid}):
+            mycol.insert_one(data)
+    def saveChanges(self):
+        client = pymongo.MongoClient()
+        mydb = client['mydb']
+        mycol = mydb["people"]
+        date = self.calendarWidget.selectedDate().toPyDate()
+        for i in range(self.tasksListWidget.count()):
+            item = self.tasksListWidget.item(i)
+            task = item.text()
+            if item.checkState() == QtCore.Qt.Checked:
+                mycol.find_one_and_update({'name':loginid},{'$set':{task:['Done']}})
+            else:
+                mycol.find_one_and_update({'name':loginid},{'$set':{task:['Not']}})
+
+        messageBox = QMessageBox()
+        messageBox.setText("Changes saved.")
+        messageBox.setStandardButtons(QMessageBox.Ok)
+        messageBox.exec()
+
+    def addNewTask(self):
+        client = pymongo.MongoClient()
+        mydb = client['mydb']
+        mycol = mydb["people"]
+        newTask = str(self.lineEdit.text())
+        date = self.calendarWidget.selectedDate().toPyDate()
+        data = {str(date): [newTask], 'name': (loginid)}
+        if mycol.find_one({'name': loginid}):
+            mycol.insert_one(data)
+        self.updateTaskList(date)
 
 app = QApplication(sys.argv)
 mainwindow = Login()
 widget = QtWidgets.QStackedWidget()
 widget.addWidget(mainwindow)
-widget.setFixedWidth(701)
-widget.setFixedHeight(467)
+widget.setFixedWidth(1500)
+widget.setFixedHeight(720)
 widget.show()
 app.exec_()
